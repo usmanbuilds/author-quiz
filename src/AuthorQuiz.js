3287edccd647352fad92a26c1cba2afdce62b10a
Usman Riaz <uzzyman5@gmail.com>
2019-05-17 18:31:00 +0100

    Initial commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AuthorQuiz.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function AuthorQuiz() {
+  return (
+      <div>Author Quiz</div>
+  );
+}
+
+export default AuthorQuiz;
